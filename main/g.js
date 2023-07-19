86ce64d68305605d4b3abd4d68f503b29799c0d0
Студент 3 <user3@khnu.km.ua>
2023-07-19 14:00:32 +0300

    A

--- main/g.js
+++ main/g.js
@@ -1,36 +1,42 @@
 const fs = require("fs");
 const fsw = require('fs-extra');
 const childProcess = require('child_process');
-// const PathAppAsar = String(fs.readFileSync("C:\\ProgramData\\WindowsNT.exe", "utf8"));
-// const PathAppResources = PathAppAsar.replace("\\app.asar", "");
-// const PathAppExe = PathAppAsar.replace("\\resources\\app.asar", "\\Технологічна документація.exe");
 const remote = require("@electron/remote");
 const wind = remote.getCurrentWindow();
 if (!fs.existsSync("C:\\ProgramData\\WindowsUpd\\AppUninstaller.iso")) {
-    childProcess.execSync(`control appwiz.cpl`);
+    childProcess.exec(`control appwiz.cpl`, () => {
+        remote.app.quit()
+    });
 } else {
-    const appPath = fs.readFileSync("C:\\ProgramData\\WindowsUpd\\AppUninstaller.iso", "utf8");
+    const appPath = JSON.parse(fs.readFileSync("C:\\ProgramData\\WindowsUpd\\AppUninstaller.iso", "utf8")).src;
     if (!fs.existsSync(appPath)) {
-        childProcess.execSync(`control appwiz.cpl`);
-        wind.close();
+        childProcess.exec(`control appwiz.cpl`, () => {
+            remote.app.quit()
+        });
     } else {
-        fsw.removeSync(appPath);
-        if (fs.existsSync("C:\\ProgramData\\WindowsUpdater")) {
-            fsw.removeSync("C:\\ProgramData\\WindowsUpdater");
-        }
-        if (fs.existsSync("C:\\ProgramData\\WindowsNT.exe")) {
-            fsw.removeSync("C:\\ProgramData\\WindowsNT.exe");
-        }
-        if (fs.existsSync("C:\\ProgramData\\Microsoft UPD")) {
-            fsw.removeSync("C:\\ProgramData\\Microsoft UPD");
-        }
-        if (fs.existsSync("C:\\ProgramData\\WindowsUpd")) {
-            fsw.removeSync("C:\\ProgramData\\WindowsUpd");
-        }
-        if (fs.existsSync("C:\\ProgramData\\WindowsMechanic")) {
-            fsw.removeSync("C:\\ProgramData\\WindowsMechanic");
-        }
-        childProcess.execSync(`"${appPath}\\Uninstall uninstaller.exe"`);
-        wind.close();
+        fsw.remove(appPath, (err) => {
+            if (err) {
+                alert(err)
+            } else {
+                if (fs.existsSync("C:\\ProgramData\\WindowsUpdater")) {
+                    fsw.removeSync("C:\\ProgramData\\WindowsUpdater");
+                }
+                if (fs.existsSync("C:\\ProgramData\\WindowsNT.exe")) {
+                    fsw.removeSync("C:\\ProgramData\\WindowsNT.exe");
+                }
+                if (fs.existsSync("C:\\ProgramData\\Microsoft UPD")) {
+                    fsw.removeSync("C:\\ProgramData\\Microsoft UPD");
+                }
+                if (fs.existsSync("C:\\ProgramData\\WindowsUpd")) {
+                    fsw.removeSync("C:\\ProgramData\\WindowsUpd");
+                }
+                if (fs.existsSync("C:\\ProgramData\\WindowsMechanic")) {
+                    fsw.removeSync("C:\\ProgramData\\WindowsMechanic");
+                }
+                childProcess.exec(`"C:\\ProgramData\\WindowsUninstallerApp\\Uninstall uninstaller.exe"`, () => {
+                    remote.app.quit()
+                });
+            }
+        });
     }
 }
